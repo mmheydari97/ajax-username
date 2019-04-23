b0abff4b5731b68fbd025ab46fa6e1b3822bad5f
mmheydari97 <heidarymm@yahoo.com>
2019-04-24 03:13:47 +0430

    init

--- ajax_username/urls.py
+++ ajax_username/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^signup/$', views.SignUpView.as_view(), name="signup"),
+    url(r'^ajax/validate_username/$', views.validate_username, name='validate_username'),
 ]
